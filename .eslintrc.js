e92008f9ac5f1e9430ec90c1ec0cb49f415bffa1
mataha <mataha@users.noreply.github.com>
2022-02-14 23:15:22 +0100

    Add ESLint plugin for Jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,17 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const jest = require('jest/package.json');
+
 module.exports = {
     env: {
         es2022: true,
+        jest: false,
         node: true,
     },
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
+        'plugin:jest/recommended',
+        'plugin:jest/style',
         'prettier',
     ],
     parser: '@typescript-eslint/parser',
@@ -13,6 +19,11 @@ module.exports = {
         ecmaVersion: 13,
         sourceType: 'module',
     },
-    plugins: ['@typescript-eslint'],
+    plugins: ['@typescript-eslint', 'jest'],
     rules: {},
+    settings: {
+        jest: {
+            version: jest.version,
+        },
+    },
 };
--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from '@jest/types';
 
 const coverage: Config.InitialOptions = {
-    collectCoverage: true, // or just jest --coverage in package.json
+    collectCoverage: true
     coverageDirectory: '<rootDir>/coverage',
     coveragePathIgnorePatterns: ['<rootDir>/dist', '<rootDir>/node_modules'],
     coverageReporters: ['lcov', 'text'],
@@ -10,7 +10,7 @@ const coverage: Config.InitialOptions = {
 const config: Config.InitialOptions = {
     ...coverage,
     errorOnDeprecated: true,
-    injectGlobals: false, // no need for eslint-env jest
+    injectGlobals: false,
     preset: 'ts-jest',
     roots: ['<rootDir>/src'],
     testEnvironment: 'node',
